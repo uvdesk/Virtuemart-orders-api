42144ddb50290124a76e5ada588c2a12275c68f0
akshay kumar <akshay.kumar758@webkul.com>
2018-04-05 19:20:33 +0530

    MySQL mis-config. fixes

--- com_wkapi_v1.0/administrator/sql/install.mysql.utf8.sql
+++ com_wkapi_v1.0/administrator/sql/install.mysql.utf8.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `#__apikeys` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `state` tinyint(1) NOT NULL DEFAULT '1',
-  `checked_out_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `checked_out_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `created_by` int(11) NOT NULL,
   `modified_by` int(11) NOT NULL,
   `domain` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
